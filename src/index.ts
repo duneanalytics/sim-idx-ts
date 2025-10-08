86b8b74590c31cb8d1f3b6d1c9890f6ddb22ecdb
Nathan McCloskey <1728509+npgm@users.noreply.github.com>
2025-10-07 21:25:16 -0400

    schema scoped tables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as db from './db';
+import { table } from './db';
 import * as App from './app';
 import * as types from './types';
 import * as middlewares from './middlewares';
 
-export { App, db, types, middlewares };
+export { App, db, types, middlewares, table };
